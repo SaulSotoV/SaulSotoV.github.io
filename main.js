a1e77df3154242fa5a5cb181565b75f62ad96251
SaulSotoV <156978352+SaulSotoV@users.noreply.github.com>
2024-02-08 02:37:01 +0000

    x

---
+++ main.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById('btnSubmit');
+btn.addEventListener("click", function(event){
+    event.preventDefault()
+if(email.value == ""){
+alert("Agrega un email")}
+if(password.value ==""){
+    alert("Agrega tu password")
+}
+})
