f390f44935cc2064b2df325dcc75087d63b66e73
SaulSotoV <156978352+SaulSotoV@users.noreply.github.com>
2024-02-14 03:53:45 +0000

    Rick&Morty

--- js/list.js
+++ js/list.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const itemList = document.getElementById("item-container"); // Cambiamos a 'item-container'
+    const template = document.getElementById("list-template");
+
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
-        itemList.appendChild(clone);
+        fetch("https://rickandmortyapi.com/api/character/")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(response => {
+                response.results.forEach((element, index) => {
+                    const clone = template.content.cloneNode(true);
+                    clone.querySelector("[data-id='number']").textContent = `${index + 1}`;
+                    clone.querySelector("[data-id='title']").textContent = element.name;
+                    clone.querySelector("[data-id='especie']").textContent = element.species;
+                    clone.querySelector("[data-id='image']").src = element.image;
+                    clone.querySelector("[data-id='gender']").textContent = element.gender;
+                    clone.querySelector("[data-id='status']").textContent = element.status;
+
+                    itemList.appendChild(clone);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching characters:', error);
+            });
     });
+
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        itemList.replaceChildren();
+        itemList.innerHTML = ""; // Vaciamos el contenido del contenedor
     });
 });
